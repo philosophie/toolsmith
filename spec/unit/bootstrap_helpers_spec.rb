44fdd6d4896d7e6cc413406d16366a5f01e5c752
Robert Ross <robert@creativequeries.com>
2013-01-18 12:18:10 -0800

    Update specs to cover flash divs.

--- spec/unit/bootstrap_helpers_spec.rb
+++ spec/unit/bootstrap_helpers_spec.rb
@@ -47,6 +47,10 @@ describe Toolsmith::ViewHelpers::BootstrapHelpers do
   end
 
   context "#flash_div" do
+    after do
+      subject.flash.clear
+    end
+
     it "renders a close link in the div" do
       subject.flash.now[:success] = "Success!"
       element = to_element(subject.flash_div(:success), "div").at_css("a.close")
@@ -60,6 +64,21 @@ describe Toolsmith::ViewHelpers::BootstrapHelpers do
         element.at_css("a").remove
         expect(element.text).to eq("#{level}!")
       end
+    end
+  end
+
+  context "#flash_divs" do
+    after do
+      subject.flash.clear
+    end
+
+    it "renders all flash divs" do
+      described_class::FLASH_LEVELS.each do |level|
+        subject.flash.now[level] = "Flash #{level}!"
+      end
+
+      divs = Nokogiri::HTML(subject.flash_divs).css("div.alert")
+      expect(divs.size).to eq(3)
     end
   end
 end
