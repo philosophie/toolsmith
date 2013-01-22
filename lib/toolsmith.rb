5a228eac34d0a7d8b8d408f32879ba35129ebb17
Brendan Loudermilk <brendan@gophilosophie.com>
2013-01-22 14:37:15 -0800

    Use double quotes

--- lib/toolsmith.rb
+++ lib/toolsmith.rb
@@ -1,7 +1,7 @@
 require "toolsmith/view_helpers"
 require "compass-rails"
 require "bootstrap-sass"
-require "toolsmith/engine" if defined?(Rails) && Rails.version >= '3.1'
+require "toolsmith/engine" if defined?(Rails) && Rails.version >= "3.1"
 
 module Toolsmith
 end
