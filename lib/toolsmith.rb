1d71ece0b346ccf526d14b386070010549a4336f
Robert Ross <robert@creativequeries.com>
2013-01-22 13:31:41 -0800

    Move engine to under compass rails and bootstrap sass.

--- lib/toolsmith.rb
+++ lib/toolsmith.rb
@@ -1,7 +1,7 @@
 require "toolsmith/view_helpers"
-require "toolsmith/engine" if defined?(Rails) && Rails.version >= '3.1'
 require "compass-rails"
 require "bootstrap-sass"
+require "toolsmith/engine" if defined?(Rails) && Rails.version >= '3.1'
 
 module Toolsmith
 end
