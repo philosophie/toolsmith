873f6f253fa72eb9770a793ee0337944e72546e4
Robert Ross <robert@creativequeries.com>
2013-01-23 11:49:28 -0800

    Assert that new_with_title receives the view context.

--- lib/toolsmith/helpers/page_header_helpers.rb
+++ lib/toolsmith/helpers/page_header_helpers.rb
@@ -1,10 +1,10 @@
 module Toolsmith
   module ViewHelpers
     module PageHeaderHelpers
-      def page_header(title, &block)
-        page_header = Toolsmith::Views::PageHeader.new_with_title(title)
-        yield page_header if block_given?
-        page_header
+      def page_header_with_buttons(title, &block)
+        header = Toolsmith::Views::PageHeader.new_with_title(self, title)
+        yield header if block_given?
+        header
       end
     end
   end
--- spec/unit/page_header_helpers_spec.rb
+++ spec/unit/page_header_helpers_spec.rb
@@ -6,15 +6,15 @@ describe Toolsmith::ViewHelpers::PageHeaderHelpers do
 
   context "#page_header" do
     before do
-      Toolsmith::Views::PageHeader.should_receive(:new_with_title).and_return(page_header)
+      Toolsmith::Views::PageHeader.should_receive(:new_with_title).with(subject, kind_of(String)).and_return(page_header)
     end
 
     it "accepts a title" do
-      expect {|b| subject.page_header "title", &b }.to yield_with_args(page_header)
+      expect {|b| subject.page_header_with_buttons "title", &b }.to yield_with_args(page_header)
     end
 
     it "returns the view object" do
-      expect(subject.page_header("title", &Proc.new {} )).to be page_header
+      expect(subject.page_header_with_buttons("title", &Proc.new {} )).to be page_header
     end
   end
 end
