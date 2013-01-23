2bc0fe7605cb075b271b0c067bf64a1566a98f60
Robert Ross <robert@creativequeries.com>
2013-01-23 13:15:41 -0800

    Add subtitle ability to page_header helper.

--- lib/toolsmith/helpers/page_header_helpers.rb
+++ lib/toolsmith/helpers/page_header_helpers.rb
@@ -1,10 +1,13 @@
 module Toolsmith
   module ViewHelpers
     module PageHeaderHelpers
-      def page_header_with_buttons(title, &block)
-        header = Toolsmith::Views::PageHeader.new_with_title(self, title)
+      def page_header(title, subtitle=nil, &block)
+        header = Toolsmith::Views::PageHeader.new(self, title, subtitle)
         yield header if block_given?
-        header
+
+        full_width_column do
+          header.to_s
+        end
       end
     end
   end
--- spec/unit/page_header_helpers_spec.rb
+++ spec/unit/page_header_helpers_spec.rb
@@ -2,19 +2,30 @@ require "spec_helper"
 
 describe Toolsmith::ViewHelpers::PageHeaderHelpers do
   subject { AbstractActionView.new }
-  let(:page_header) { double("page_header", to_s: "asd") }
+  let(:page_header) { double("page_header", to_s: "content") }
+
+  before do
+    Toolsmith::Views::PageHeader.should_receive(:new).with(subject, kind_of(String), anything).and_return(page_header)
+  end
 
   context "#page_header" do
-    before do
-      Toolsmith::Views::PageHeader.should_receive(:new_with_title).with(subject, kind_of(String)).and_return(page_header)
-    end
-
     it "accepts a title" do
-      expect {|b| subject.page_header_with_buttons "title", &b }.to yield_with_args(page_header)
+      expect {|b| subject.page_header "title", &b }.to yield_with_args(page_header)
     end
 
-    it "returns the view object" do
-      expect(subject.page_header_with_buttons("title", &Proc.new {} )).to be page_header
+    it "accepts a subtitle" do
+      expect {|b| subject.page_header "title", "subtitle", &b }.to yield_with_args(page_header)
+    end
+
+    context "renders" do
+      it "a full width column" do
+        expect(subject.page_header("title")).to have_tag "div.row div.span12"
+      end
+
+      it "a header within the full width column" do
+        markup = subject.page_header("title").to_s.to_element
+        expect(markup.at_css("div.span12").inner_html).to eq(page_header.to_s)
+      end
     end
   end
 end
