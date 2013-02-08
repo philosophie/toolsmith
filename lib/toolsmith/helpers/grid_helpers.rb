a7325eccf828899a30823070a317a05ddabaeb30
Robert Ross <robert@creativequeries.com>
2013-02-07 18:22:50 -0800

    Update yardopts to use markdown as markup format. Update grid helpers documentation. Set the standard. Update gitignore to include doc folders.

--- lib/toolsmith/helpers/grid_helpers.rb
+++ lib/toolsmith/helpers/grid_helpers.rb
@@ -2,28 +2,45 @@ module Toolsmith
   module ViewHelpers
     module GridHelpers
 
-      # Returns a bootstrap div for rows
+      # Generates a grid row.
+      #
+      # ```
       # <%= row do %>
       #   Content
       # <% end %>
+      # ```
+      # Will output:
       #
+      # ```
+      # <div class="row">
+      #   Content
+      # </div>
+      # ```
       # @param options [Hash]
-      # @param block [Proc]
-      # @return [String] <div class="row">Content</div>
+      # @return [String] `<div class="row">Content</div>`
       def row(options={}, &block)
         row_class = options[:fluid] ? "row-fluid" : "row"
         content_tag(:div, class: row_class, &block)
       end
 
       # Returns a grid column element.
+      #
+      # ```
       # <%= column 12 do %>
       #   I'm in a column!
       # <% end %>
+      # ```
+      # Will output:
+      #
+      # ```
+      # <div class="span12">
+      #   I'm in a column!
+      # </div>
+      # ```
       #
       # @param width [Fixnum]
       # @param options [Hash]
-      # @param block [Proc]
-      # @return [String] <div class="span12">I'm in a column!</div>
+      # @return [String]
       def column(width, options = {}, &block)
         classes = %W[span#{width}]
         classes << "offset#{options[:offset]}" if options[:offset]
@@ -32,6 +49,21 @@ module Toolsmith
       end
 
       # Create a row and fullwidth column (based on the standard 12 wide)
+      #
+      # ```
+      # <%= full_width_column do %>
+      #   My more-to-love column.
+      # <% end %>
+      # ```
+      # Will output:
+      #
+      # ```
+      # <div class="row">
+      #   <div class="span12">
+      #     My more-to-love column.
+      #   </div>
+      # </div>
+      # ```
       # @see #row
       # @see #column
       def full_width_column(options={}, &block)
