ff12346f274d46ec83dcb2821071d4c4aac65ac0
Robert Ross <robert@creativequeries.com>
2013-02-07 17:35:10 -0800

    Add documentation for grid helpers.

--- lib/toolsmith/helpers/grid_helpers.rb
+++ lib/toolsmith/helpers/grid_helpers.rb
@@ -1,11 +1,29 @@
 module Toolsmith
   module ViewHelpers
     module GridHelpers
+
+      # Returns a bootstrap div for rows
+      # <%= row do %>
+      #   Content
+      # <% end %>
+      #
+      # @param options [Hash]
+      # @param block [Proc]
+      # @return [String] <div class="row">Content</div>
       def row(options={}, &block)
         row_class = options[:fluid] ? "row-fluid" : "row"
         content_tag(:div, class: row_class, &block)
       end
 
+      # Returns a grid column element.
+      # <%= column 12 do %>
+      #   I'm in a column!
+      # <% end %>
+      #
+      # @param width [Fixnum]
+      # @param options [Hash]
+      # @param block [Proc]
+      # @return [String] <div class="span12">I'm in a column!</div>
       def column(width, options = {}, &block)
         classes = %W[span#{width}]
         classes << "offset#{options[:offset]}" if options[:offset]
@@ -13,6 +31,9 @@ module Toolsmith
         content_tag(:div, class: classes,  &block)
       end
 
+      # Create a row and fullwidth column (based on the standard 12 wide)
+      # @see #row
+      # @see #column
       def full_width_column(options={}, &block)
         row(options) do
           column(12, &block)
